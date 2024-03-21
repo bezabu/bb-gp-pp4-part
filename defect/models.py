18bae3a69c4a622eb371e5520316544f3d004299
Ben Butler <benbutleranimator@gmail.com>
2024-03-21 14:42:04 +0000

    Edit defect model, add methods

--- defect/models.py
+++ defect/models.py
@@ -14,13 +14,15 @@ class Category(models.Model):
     fa_string = models.SlugField(max_length=100)
     colour = models.CharField(max_length=7)
 
+    def __str__(self):
+        return f"Category: {self.name}"
 
 
 class Defect(models.Model):
     """
     Stores a single defect, related to :model:'Category' and :model:'auth.User'
     """
-    defect_id = models.IntegerField(primary_key=True)
+    defect_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200, unique=True)
     category = models.ForeignKey(
         Category, on_delete=models.CASCADE, related_name="defects")
@@ -30,7 +32,12 @@ class Defect(models.Model):
     #image_url = models.SlugField()
     reported_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    reoccurance = models.IntegerField()
-    updates_count = models.IntegerField()
-    latest_update = models.DateTimeField()
+    reoccurance = models.IntegerField(default=0)
+    updates_count = models.IntegerField(default=0)
+    latest_update = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ["-reported_on"]
+
+    def __str__(self):
+        return f"Defect: {self.title} ({self.category})"
