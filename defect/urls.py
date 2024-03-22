bf2a03911fa50b96204655fa4ae27131793d8295
Ben Butler <benbutleranimator@gmail.com>
2024-03-22 14:21:05 +0000

    fix url tags in navbar

--- defect/urls.py
+++ defect/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('defect_list/', views.DefectList.as_view(), name='def_list'),
     path('category_list/', views.CategoryList.as_view(), name='cat_list'),
+    path('dashboard/', views.DashList.as_view(), name='dash_list'),
 ]
--- defect/views.py
+++ defect/views.py
@@ -11,3 +11,7 @@ class DefectList(generic.ListView):
 class CategoryList(generic.ListView):
     queryset = Category.objects.all()
     template_name = "category_list.html"
+
+class DashList(generic.ListView):
+    queryset = Defect.objects.all().order_by("-reported_on")[:2]
+    template_name = "dash.html"
