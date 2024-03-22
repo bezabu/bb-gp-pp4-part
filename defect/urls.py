088e17df74751e68c0cf86c69e2720f4ffab80e5
Ben Butler <benbutleranimator@gmail.com>
2024-03-22 20:20:48 +0000

    Add defect detail view & template

--- defect/urls.py
+++ defect/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('defect_list/', views.defect_list, name='def_list'),
     path('category_list/', views.CategoryList.as_view(), name='cat_list'),
     path('dashboard/', views.dashboard, name='dash_list'),
+    path('<defect_id>/', views.defect_detail, name='def_detail'),
 ]
--- defect/views.py
+++ defect/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from .models import Defect, Category
+from .models import Defect, Category, Update
 
 # Create your views here.
 
@@ -9,9 +9,11 @@ def_per_page = 10
 def defect_list(request):
     defects = Defect.objects.all()
     categorys = Category.objects.all()
+    #update_count = defects.updates.count()
     context = {
         'categorys': categorys,
-        'defects': defects
+        'defects': defects,
+
     }
     paginate_by = def_per_page
     return render(
@@ -20,13 +22,38 @@ def defect_list(request):
 def dashboard(request):
     defects = Defect.objects.all().order_by("-reported_on")[:10]
     categorys = Category.objects.all()
+
     context = {
         'categorys': categorys,
-        'defects': defects
+        'defects': defects,
     }
     paginate_by = def_per_page
     return render(
         request, 'defect/dash.html', context)
+
+def defect_detail(request, defect_id):
+    queryset = Defect.objects.all()
+    defects = get_object_or_404(queryset, defect_id=defect_id)
+    categorys = Category.objects.all()
+    updates = Update.objects.all()
+    #update_count = defect.updates.count()
+    context = {
+        'categorys': categorys,
+        'defects': defects,
+        'updates': updates,
+    }
+    paginate_by = def_per_page
+
+    return render(
+        request,
+        'defect/defect_detail.html',
+        {  
+            'categorys': categorys,
+            'defects': defects,
+            'updates': updates,
+            #"update_count": update_count,
+        },
+    )
 
 class CategoryList(generic.ListView):
     queryset = Category.objects.all()
